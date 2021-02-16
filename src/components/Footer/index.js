4983eff45cd72ed0b6a3e0b0e42a4ec3f3c18c1b
clemencelemenager <clemence.lemenager@gmail.com>
2021-02-16 13:40:58 +0100

    Update link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ import './footer.scss';
 
 const Footer = () => (
   <div className="footer">
-    <a href="" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/clemencelemenager/MeteoPlage-React" target="_blank" rel="noopener noreferrer">
       Voir le projet sur Github
     </a>
   </div>
